ea34c3bbfbdd144559c80673ce6a5b630a3128cd
Nate Mills <natemills018@gmail.com>
2024-07-24 19:19:46 -0400

    Initial commit for working vite_tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  const handleButton = () => {
+    if(document.body.classList.contains('dark')) {
+      document.body.classList.remove('dark');
+    } else {
+      document.body.classList.add('dark')
+    }
+     
+
+  }
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <h1>Vite + React</h1>
+
+      <button className='bg-indigo-400 rounded-sm'>Click me!</button>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="text-6xl font-bold underline">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
