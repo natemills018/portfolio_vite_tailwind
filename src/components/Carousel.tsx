2a8c26d94503b9e903e2bec0351fdcd209114ba7
Nate Mills <natemills018@gmail.com>
2024-07-25 21:50:03 -0400

    Adding carousel idea in the bottom of the page

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+
+interface CarouselProps {
+
+}
+
+
+const Carousel = (props: CarouselProps) => {
+    return(
+        <div className="carousel rounded-box">
+            <div className="carousel-item">
+                
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Carousel
