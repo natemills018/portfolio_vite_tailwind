6f3ad44f4830b35de5880019505d1236176fd17c
Nate Mills <natemills018@gmail.com>
2024-07-27 00:07:16 -0400

    Completed About Me paragraph

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,32 +1,24 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-interface AboutMeProps {
-
-}
-
-
+interface AboutMeProps {}
 
 const AboutMe = (props: AboutMeProps) => {
+  // if I want some sort of state or fetching hook, it goes here!
 
-
-    // if I want some sort of state or fetching hook, it goes here!
-
-
-
-    return (
-        <div>
-
-            <main className="">
-                <button  className="btn"></button>
-            </main>
-
-        </div>
-    )
-
-
-
-}
-
+  return (
+    <div>
+      <main className="">
+        <h1>About Me!</h1>
+        <p>
+          Hello, My name is Nate Mills! Full stack web developer with a passion
+          for tech and learning new frameworks. Proficient in JavaScript,
+          TypeScript, React, Express, SQL and Node. I'm focused on finding
+          solutions to complex problems, and creating impactful software.
+        </p>
+      </main>
+    </div>
+  );
+};
 
 export default AboutMe;
